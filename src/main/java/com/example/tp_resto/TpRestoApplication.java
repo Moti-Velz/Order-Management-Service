f9b18dd86ee81c936ae8ff1551012bbfa55b5c5f
Tim <roppa070@gmail.com>
2023-05-23 14:43:54 -0400

    Initial Commit

---
+++ src/main/java/com/example/tp_resto/TpRestoApplication.java
@@ -0,0 +1,13 @@
+package com.example.tp_resto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpRestoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpRestoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tp_resto/TpRestoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tp_resto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpRestoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
