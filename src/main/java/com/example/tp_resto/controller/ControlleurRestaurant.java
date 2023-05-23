246ead06be0f897ec125b2d67878b9ddc48719c2
Francis Mercier <arkilar_23@hotmail.com>
2023-05-23 16:59:17 -0400

    manipulation des classe Entity

--- src/main/java/com/example/tp_resto/controller/ControlleurRestaurant.java
+++ src/main/java/com/example/tp_resto/controller/ControlleurRestaurant.java
@@ -1,4 +1,17 @@
 package com.example.tp_resto.controller;
 
+import com.example.tp_resto.service.RestaurantService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ControlleurRestaurant {
+
+    private RestaurantService restaurantService;
+    @Autowired
+    public ControlleurRestaurant(RestaurantService restaurantService){this.restaurantService= restaurantService;}
+
+    @GetMapping("/")
+    public String sayHello(){return "Hello world";}
 }
