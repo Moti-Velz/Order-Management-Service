99e7bee24521d81dcd08161f8d848ea407fa757d
Tim <roppa070@gmail.com>
2023-06-14 13:48:50 -0400

    Mise en place des Batteries de Tests Pour les Controlleur Suivants : ControlleurCommande, ControlleurMenuItem

---
+++ src/test/java/com/example/tp_resto/controller/ControlleurMenuItemTest.java
@@ -0,0 +1,38 @@
+package com.example.tp_resto.controller;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ControlleurMenuItemTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getAllMenuItems() {
+    }
+
+    @Test
+    void getMenuItemById() {
+    }
+
+    @Test
+    void addMenuItem() {
+    }
+
+    @Test
+    void updateMenuItem() {
+    }
+
+    @Test
+    void deleteMenuItemById() {
+    }
+}
