06ad286e42373a9408b2d622e4e2cd96c47cad53
Tim <roppa070@gmail.com>
2023-06-14 22:54:18 -0400

    DTO

---
+++ src/main/java/com/example/tp_resto/dto/visualisationCommandeDTO.java
@@ -0,0 +1,8 @@
+package com.example.tp_resto.dto;
+
+/**
+ * Ce DTO sert a récupérer les informations necessaires à la visualisation du commande par le frontend
+ * idCommande, []MenuItem avec qte associée sans id, orderTime, billTime, sous-total, prix Total
+ */
+public class visualisationCommandeDTO {
+}
